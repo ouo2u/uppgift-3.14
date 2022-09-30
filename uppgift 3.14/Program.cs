1d9d719b53f90ee2d5e4972788879297fa841ac9
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-30 11:02:57 +0200

    Add project files.

---
+++ uppgift 3.14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Uppgift_3._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.WriteLine("har du gått gymnaiset? svara med j för ja och n för nej");
+            string rättsvar= Console.ReadLine();
+
+            Console.WriteLine("Hur gammal är du");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (rättsvar == "j" && ålder < 22)
+            {
+                Console.WriteLine("vi vill gärna anställda dig");
+            }
+            else
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu ");
+            
+
+
+
+
+
+
+
+        }
+    }
+}
